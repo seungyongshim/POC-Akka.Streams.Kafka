e74d2be69d3b8a7a0f2c9babebde364964752f20
심승용 <seungyong.shim@mirero.co.kr>
2021-02-09 12:57:56 +0900

    테스트 수정

---
+++ src/Common/KafkaSenderActor.cs
@@ -0,0 +1,40 @@
+using System;
+using Akka.Actor;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+using Akka.Streams.Kafka.Dsl;
+using Akka.Streams.Kafka.Messages;
+using Akka.Streams.Kafka.Settings;
+using Confluent.Kafka;
+
+namespace Common
+{
+    public class KafkaSenderActor : ReceiveActor
+    {
+        public KafkaSenderActor(string topic)
+        {
+            var producerSettings = ProducerSettings<Null, string>.Create(Context.System, null, null)
+                .WithBootstrapServers("localhost:9092");
+
+            var source = Source.ActorRef<object>(5000, OverflowStrategy.DropTail);
+
+            var graph = GraphDsl.Create(source, (builder, start) =>
+            {
+                var sink = builder.Add(KafkaProducer.PlainSink(producerSettings));
+                var flow = builder.Add(from msg in Flow.Create<object>()
+                                       select new ProducerRecord<Null, string>(topic, msg.ToString()));
+
+
+                builder.From(start)
+                       .Via(flow)
+                       .To(sink);
+
+                return ClosedShape.Instance;
+            });
+
+            var sourceActor = Context.Materializer().Materialize(graph);
+
+            Receive<string>(m => sourceActor.Forward(m));
+        }
+    }
+}
--- src/Tests/UnitTest1.cs
+++ src/Tests/UnitTest1.cs
@@ -1,14 +1,51 @@
 using System;
+using Akka.Actor;
+using Common;
+using FluentAssertions.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexon.Akka.DependencyInjection;
 using Xunit;
 
 namespace Tests
 {
-    public class UnitTest1
+    public class KafkaSpec
     {
         [Fact]
-        public void Test1()
+        public async void Simple()
         {
+            string topic = "kafka.spec.simple.test";
+            // arrange
+            using var host =
+                Host.CreateDefaultBuilder()
+                    .UseAkka("test", @"
+                    akka.kafka.producer {
+                       parallelism = 100
+                       flush-timeout = 10s
+                       use-dispatcher = ""akka.kafka.default-dispatcher""
+                    }
+                    akka.kafka.default-dispatcher {
+                       type = ""Dispatcher""
+                       executor = ""default-executor""
+                    }
+                    ", sys =>
+                    {
+                        sys.ActorOf(sys.PropsFactory<KafkaSenderActor>()
+                                       .Create(topic), "KafkaSender");
+                    })
+                    .UseAkkaWithXUnit2()
+                    .Build();
 
+            await host.StartAsync();
+
+            var sys = host.Services.GetService<ActorSystem>();
+
+            var kafkaSenderActor = await sys.ActorSelection("/user/KafkaSender")
+                                            .ResolveOne(10.Seconds());
+
+            kafkaSenderActor.Tell("Hello, Test!!!");
+
+            await host.StopAsync();
         }
     }
 }
